703d33d89d88480e2edff93005ba9aa3e5a0abb5
abdulwahab3202 <abdulwahab142006@gmail.com>
2026-01-12 22:17:38 +0530

    change in booking client

--- backend/booking_service/src/main/java/com/fullstack/booking_service/BookingServiceApplication.java
+++ backend/booking_service/src/main/java/com/fullstack/booking_service/BookingServiceApplication.java
@@ -3,10 +3,10 @@ package com.fullstack.booking_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableMongoAuditing
+@EnableFeignClients(basePackages = "com.fullstack.booking_service.client")
 @EnableDiscoveryClient
 public class BookingServiceApplication {
 
